0b2157dd60ffe441959329732d60081c468f048e
tBilby <163959154+tBilby@users.noreply.github.com>
2024-03-19 14:46:59 +1100

    Minor updates to ThreeGaussianBasic.py

--- examples/ThreeGaussianBasic.py
+++ examples/ThreeGaussianBasic.py
@@ -1,5 +1,4 @@
-from context import tbilby # this is just a hack to make the example work, usually import tbilby should work
-import bilby
+import tbilby, bilby
 from bilby.core.prior import  Uniform
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,30 +10,30 @@ from scipy.stats import norm
 
 
 # model 
-n_peaks=8
+n_peaks = 8
 sigma = 10
-Amp_noise=0.05
+Amp_noise = 0.05
 def gauss(x,mu,sigma_g):
     return norm(mu,sigma_g).pdf(x)
-# here we define a model which could be a sum of several functions.. 
-componant_functions_dict={}
-componant_functions_dict[gauss]=(n_peaks,'mu')
-model = tbilby.core.base.create_transdimensional_model('model',  componant_functions_dict,returns_polarization=False,SaveTofile=True)
+# here we define a model which could be a sum of several functions. 
+component_functions_dict = {}
+component_functions_dict[gauss] = (n_peaks,'mu')
+model = tbilby.core.base.create_transdimensional_model('model', component_functions_dict,returns_polarization=False ,SaveTofile=True)
 
 # create some data to work with 
 random.seed(10)
-x=np.linspace(0,100,1001)
-p=np.array([54, 15, 81])
+x = np.linspace(0, 100, 1001)
+p = np.array([54, 15, 81])
 #add some noise
-noise=norm(loc=0,scale=Amp_noise).rvs(1001)
-y =   gauss(x,p[0],sigma) +  gauss(x,p[1],sigma) +  gauss(x,p[2],sigma) + noise
+noise=norm(loc=0, scale = Amp_noise).rvs(1001)
+y = gauss(x, p[0], sigma) + gauss(x, p[1], sigma) + gauss(x, p[2], sigma) + noise
 
 # priors 
 priors_t = bilby.core.prior.dict.ConditionalPriorDict()
 priors_t['n_gauss'] = tbilby.core.prior.DiscreteUniform(0,n_peaks,'n_gauss')
-priors_t['sigma_g']= sigma # set it constant so we wont sample on it 
-# creates a list of simple unifrom priros for you..
-priors_t =tbilby.core.base.create_plain_priors(Uniform, 'mu', nmax=n_peaks,prior_dict_to_add=priors_t, minimum=0,maximum=100)
+priors_t['sigma_g'] = sigma # set constant so we don't sample it 
+# create a list of simple uniform priors
+priors_t = tbilby.core.base.create_plain_priors(Uniform, 'mu', nmax=n_peaks,prior_dict_to_add=priors_t, minimum=0,maximum=100)
 
 samples = priors_t.sample(size=50000)
 # just to see that the conditional works 
@@ -44,7 +43,7 @@ plt.plot(samples['mu0'],samples['mu1'],'o')
 plt.xlabel('mu0')
 plt.ylabel('mu1')
 
-likelihood = bilby.likelihood.GaussianLikelihood(x, y, model, sigma=Amp_noise*np.ones(len(x)))
+likelihood = bilby.likelihood.GaussianLikelihood(x, y, model, sigma = Amp_noise*np.ones(len(x)))
 
 # run the smapler 
 result = bilby.core.sampler.run_sampler(
@@ -58,9 +57,7 @@ result = bilby.core.sampler.run_sampler(
        
     )
 
-
-    
-# lets check the best number of componant, this is equivalent to comparing BF   
+# lets check the best number of component, this is equivalent to comparing BF   
 plt.figure()
 plt.hist(result.posterior['n_gauss'].astype(int),bins=np.arange(9))
 plt.xlabel('n components')
